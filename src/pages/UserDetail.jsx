431c3b1d5a7243b157add760db36626558b96120
ASWINTK84 <tkaswin0911@gmail.com>
2025-09-30 23:30:05 +0530

    aaa

--- src/pages/UserDetail.jsx
+++ src/pages/UserDetail.jsx
@@ -91,7 +91,12 @@ export default function UserDetail() {
     }
   };
 
-  if (!user) return <div>Loading...</div>;
+  if (!user) return <div className="flex items-center justify-center min-h-screen bg-gray-50">
+        <div className="flex flex-col items-center space-y-3">
+          <div className="w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+          <p className="text-xl font-semibold text-gray-700">Loading ...</p>
+        </div>
+      </div>;
 
 
 
